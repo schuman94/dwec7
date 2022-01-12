71aeed851d2694ddc1d2a92f7c611482c6e06b41
sergio <sergio.chulian@iesdonana.org>
2022-01-12 21:03:06 +0100

    sin terminar ej 1

---
+++ ej7A01/ej7A01.js
@@ -0,0 +1,11 @@
+const boton = document.getElementById('cambiaContenido');
+
+function cambiarContenido() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'holamundoS.txt');
+
+    xhr.onreadystatechange = function () {
+        
+    }
+}
