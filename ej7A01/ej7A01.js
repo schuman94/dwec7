e7647e8b77142a25500657d2594b572b22717199
sergio <sergio.chulian@iesdonana.org>
2022-01-13 18:07:48 +0100

    ej1 hecho

--- ej7A01/ej7A01.js
+++ ej7A01/ej7A01.js
@@ -1,11 +1,12 @@
-const boton = document.getElementById('cambiaContenido');
+document.getElementById('cambiaContenido').addEventListener("click", cambiaContenido);
 
-function cambiarContenido() {
+function cambiaContenido() {
     const xhr = new XMLHttpRequest();
-
-    xhr.open('GET', 'holamundoS.txt');
-
     xhr.onreadystatechange = function () {
-        
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('parrafo').innerHTML = this.responseText;
+        }
     }
+    xhr.open('GET', 'holamundo.txt', true);
+    xhr.send();
 }
